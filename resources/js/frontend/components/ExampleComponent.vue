80481f749fbc9b824c4004331477fd313776d5ed
Kantai <kantai.developer@gmail.com>
2021-05-07 11:04:38 +0800

    init
    init

---
+++ resources/js/frontend/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <span>Laravel Typical High-Load Example</span>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
